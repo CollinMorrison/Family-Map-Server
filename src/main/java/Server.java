78117a5121c0e96f818c25629446525e3160ec55
Collin Morrison <morricollin@gmail.com>
2023-03-11 14:46:13 -0700

    Server class complete

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,7 +11,6 @@ public class Server {
 
     private void run(String portNumber) {
         System.out.println("Initializing HTTP Server");
-
         try {
             server = HttpServer.create(
                     new InetSocketAddress(Integer.parseInt(portNumber)),
@@ -21,27 +20,28 @@ public class Server {
             e.printStackTrace();
             return;
         }
-
         server.setExecutor(null);
 
         System.out.println("Creating contexts");
 
         server.createContext("/user/register", new RegisterHandler());
-
         server.createContext("/user/login", new LoginHandler());
-
         server.createContext("/clear", new ClearHandler());
-
         server.createContext("/fill/", new FillHandler());
-
         server.createContext("/load", new LoadHandler());
-
         server.createContext("/person/", new PersonIDHandler());
-
         server.createContext("/person", new PersonHandler());
-
         server.createContext("/event/", new EventIDHandler());
-
         server.createContext("/event", new EventHandler());
+        server.createContext("/", new FileHandler());
+
+        System.out.println("Starting server");
+        server.start();
+        System.out.println("Server started");
+    }
+
+    public static void main(String[] args) {
+        String portNumber = args[0];
+        new Server().run(portNumber);
     }
 }
